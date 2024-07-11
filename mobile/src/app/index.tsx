12a5e529bcd2073cb8d912117268cde732694f9d
Laís Brum Menezes <laisbrme@gmail.com>
2024-07-11 16:51:22 -0300

    comit initial

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Index(){
+    return (
+        <View>
+            <Text>Welcome to React Native!</Text>
+        </View>
+    )
+}
